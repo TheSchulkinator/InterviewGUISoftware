01babeb4da7d80b3ec156e70b9deb4fe89c7a33a
Unknown <Gregschulkey@gmail.com>
2018-07-03 07:21:26 -0400

    Feature: added binding between view and viewmodel

--- InterviewGUISoftware/ViewModel/BaseViewModel.cs
+++ InterviewGUISoftware/ViewModel/BaseViewModel.cs
@@ -4,11 +4,82 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InterviewGUISoftware.Model;
+using Prism.Commands;
+using System.Windows.Input;
+using InterviewGUISoftware.Model;
 
 namespace InterviewGUISoftware.ViewModel
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        private int tMinValue;
+        private int tMaxValue;
+        private int tMax = TimeFilter.testObjects.Count;
+        private List<MaxMinAvgModel> maxMinAvgList;
+
         public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        //set the max and min time for the filter
+        public string TestMaxHintString
+        {
+            get { return "tMax: " + TimeFilter.testObjects.Count.ToString(); }
+            set { }  
+        } 
+        public string TestMinHintString
+        {
+            get { return "tMin: 0"; }
+            set { }
+        }
+
+        //get text from textbox on submit 
+        public int TMinValue
+        {
+            get { return tMinValue; }
+            set
+            {
+                tMinValue = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(TMinValue)));
+            }
+        }
+        public int TMaxValue
+        {
+            get { return tMaxValue; }
+            set
+            {
+                tMaxValue = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(TMaxValue)));
+            }
+        }
+
+        //Add click command and validation
+        public ICommand SubmitButtonCommand
+        {
+            get { return new DelegateCommand<object>(FuncToCall, FuncToEvaluate); }
+        }
+
+        private void FuncToCall(object context)
+        {
+            maxMinAvgList = TimeFilter.filterForTime(tMaxValue, tMinValue);
+            PropertyChanged(this, new PropertyChangedEventArgs(nameof(MinMaxAvgModelData)));
+        }
+
+        private bool FuncToEvaluate(object context)
+        {
+            if (tMinValue >= 0 && tMaxValue <= tMax && tMinValue <= tMaxValue)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Bind Datagrid 
+        public List<MaxMinAvgModel> MinMaxAvgModelData
+        {
+            get { return maxMinAvgList; }
+            set { }
+        }
+
+
     }
 }
