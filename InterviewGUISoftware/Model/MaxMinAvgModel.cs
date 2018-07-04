9bc4425e0189887c788772f7e513a9d057fe6040
Unknown <Gregschulkey@gmail.com>
2018-07-03 21:41:44 -0400

    Bug: Datagrid now binding to model class.
    Changed vars to properties in order to bind data

--- InterviewGUISoftware/Model/MaxMinAvgModel.cs
+++ InterviewGUISoftware/Model/MaxMinAvgModel.cs
@@ -9,20 +9,21 @@ namespace InterviewGUISoftware.Model
     public class MaxMinAvgModel
     {
         //vars to store the max min and avg for test results
-        public string test;
-        public decimal max;
-        public decimal min;
-        public decimal avg;
+       
+        public string Test { get; set; }
+        public decimal Max { get; set; }
+        public decimal Min { get; set; }
+        public decimal Avg { get; set; }
 
         public static MaxMinAvgModel maxMinAvgObject(string valueTest, decimal valueMax, decimal valueMin,
             decimal valueAvg)
         {
             //store values into model to load into datagrid
             MaxMinAvgModel maxMinAvgObject = new MaxMinAvgModel();
-            maxMinAvgObject.test = valueTest;
-            maxMinAvgObject.max = valueMax;
-            maxMinAvgObject.min = valueMin;
-            maxMinAvgObject.avg = valueAvg;
+            maxMinAvgObject.Test = valueTest;
+            maxMinAvgObject.Max = valueMax;
+            maxMinAvgObject.Min = valueMin;
+            maxMinAvgObject.Avg = valueAvg;
 
             return maxMinAvgObject;
         }
--- InterviewGUISoftware/ViewModel/BaseViewModel.cs
+++ InterviewGUISoftware/ViewModel/BaseViewModel.cs
@@ -80,6 +80,5 @@ namespace InterviewGUISoftware.ViewModel
             set { }
         }
 
-
     }
 }
